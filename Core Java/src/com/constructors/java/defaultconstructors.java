74e5dfdf7871651f4e7ceff6366465adaf7941ed
aitha <aitha@LAVANYA_REDDY>
2026-08-29 13:52:40 +0530

    Constructors

---
+++ Core Java/src/com/constructors/java/defaultconstructors.java
@@ -0,0 +1,27 @@
+package com.constructors.java;
+
+public class defaultconstructors {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		System.out.println("Name : " + s.name + "   rollNo : "+ s.rollNo);
+	}
+}
+
+		class Student{
+			int rollNo=5;
+			String name="Lavanya";
+			//Default Constructor
+			Student(){
+				
+				
+				
+				
+			}
+			
+				
+			}
+		
+	
+
+
---
+++ Core Java/src/com/constructors/java/parameterizedconstructors.java
@@ -0,0 +1,26 @@
+package com.constructors.java;
+
+public class parameterizedconstructors {
+
+	public static void main(String[] args) {
+		
+		        Student1 s = new Student1(101, "Lavanya");
+
+		        System.out.println("Name : " + s.name + " RollNo : " + s.rollNo);
+		    }
+		}
+
+		class Student1 {
+
+		    int rollNo;
+		    String name;
+
+		    // Parameterized Constructor
+		    Student1(int r, String n) {
+		        rollNo = r;
+		        name = n;
+		    }
+		}
+	
+
+
